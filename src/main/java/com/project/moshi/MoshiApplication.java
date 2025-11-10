2e24b851e0c8f8f03a7ccf42550474b95d88eb70
Xiaoyu Liu <xiaoyuliu@Optimus-MacBook-Air.local>
2025-11-10 18:46:17 -0500

    Initial project

---
+++ src/main/java/com/project/moshi/MoshiApplication.java
@@ -0,0 +1,13 @@
+package com.project.moshi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoshiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoshiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/moshi/MoshiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.moshi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoshiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
